684d1db72517dde591366f04c205afb25db34490
Momin Tariq <Momin.Tariq@finra.org>
2018-03-01 17:18:20 -0500

    displaying conditional content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Momin', age: 26 },
+      { name: 'Eiman', age: 20 },
+      { name: 'Mahnoor', age: 18 }
+    ],
+    otherState: 'some other value'
+  };
+  
+  switchNameHandler = (newName) => {
+    // console.log('was clicked');
+    // DO NOT DO THIS: this.state.persons[0].name = 'Momin Tariq';
+    this.setState({
+      persons: [
+        { name: newName, age: 26 },
+        { name: 'Eiman', age: 20 },
+        { name: 'Mahnoor', age: 18 }
+      ]
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Momin', age: 26 },
+        { name: event.target.value, age: 20 },
+        { name: 'Mahnoor', age: 18 }
+      ],
+      otherState: 'some other state',
+      showPersons: false
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age}/>
+          })}
+      </div>
+      );
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button
+          style={style} 
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        
+        {persons}
+        
+        {/* 
+          this.state.showPersons ?
+            <div>
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}/>
+              <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this,'Momin Tariq!!!')}
+                changed={this.nameChangedHandler}>Hobbies: sleeping</Person>
+              <Person 
+                name={this.state.persons[2].name} 
+                age={this.state.persons[2].age}/>
+            </div> : null
+        */}
       </div>
     );
   }
